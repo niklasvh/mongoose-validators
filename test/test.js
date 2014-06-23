67c129656fda39c3c831ae1f2d38f7fa12cf56d9
Niklas von Hertzen <niklasvh@gmail.com>
2014-06-23 21:25:48 +0300

    Add additional unit tests

--- test/test.js
+++ test/test.js
@@ -86,5 +86,164 @@ describe('Validators', function() {
                 done();
             });
         });
-    })
+    });
+
+    describe('.contains', function() {
+        it('Should pass string containing seed', function (done) {
+            items.path('text').validate(validators.contains("world"));
+            new model({text: "hello world!"}).save(function (err) {
+                assert.equal(err, null);
+                done();
+            });
+        });
+
+        it('Should fail string not containing seed', function (done) {
+            items.path('text').validate(validators.contains("cuicca"));
+            new model({text: "hello world!"}).save(function (err) {
+                assert.equal(err.message, "Validation failed");
+                done();
+            });
+        });
+    });
+
+    describe('.matches', function() {
+        it('Should pass string matching pattern', function (done) {
+            items.path('text').validate(validators.matches(/ORLD/i));
+            new model({text: "hello world!"}).save(function (err) {
+                assert.equal(err, null);
+                done();
+            });
+        });
+
+        it('Should pass string matching pattern with modifier', function (done) {
+            items.path('text').validate(validators.matches("ORLD", "i"));
+            new model({text: "hello world!"}).save(function (err) {
+                assert.equal(err, null);
+                done();
+            });
+        });
+
+        it('Should fail string not matching pattern', function (done) {
+            items.path('text').validate(validators.matches(/CUICCA/));
+            new model({text: "digicuicca"}).save(function (err) {
+                assert.equal(err.message, "Validation failed");
+                done();
+            });
+        });
+    });
+
+    describe('.isEmail', function() {
+        it('Should pass string with valid email', function (done) {
+            items.path('text').validate(validators.isEmail());
+            new model({text: "hello@example.com"}).save(function (err) {
+                assert.equal(err, null);
+                done();
+            });
+        });
+
+        it('Should fail string with invalid email', function (done) {
+            items.path('text').validate(validators.isEmail());
+            new model({text: "hello@example"}).save(function (err) {
+                assert.equal(err.message, "Validation failed");
+                done();
+            });
+        });
+    });
+
+    describe('.isURL', function() {
+        it('Should pass string with valid URL', function (done) {
+            items.path('text').validate(validators.isURL());
+            new model({text: "example.com"}).save(function (err) {
+                assert.equal(err, null);
+                done();
+            });
+        });
+
+        it('Should fail string with invalid URL and options', function (done) {
+            items.path('text').validate(validators.isURL({require_protocol: true}));
+            new model({text: "example.com"}).save(function (err) {
+                assert.equal(err.message, "Validation failed");
+                done();
+            });
+        });
+
+        it('Should fail string with invalid URL', function (done) {
+            items.path('text').validate(validators.isURL());
+            new model({text: "invalid/"}).save(function (err) {
+                assert.equal(err.message, "Validation failed");
+                done();
+            });
+        });
+    });
+
+
+    describe('.isIP', function() {
+        it('Should pass string with valid ip (4)', function (done) {
+            items.path('text').validate(validators.isIP());
+            new model({text: "127.0.0.1"}).save(function (err) {
+                assert.equal(err, null);
+                done();
+            });
+        });
+
+        it('Should pass string with valid ip (6)', function (done) {
+            items.path('text').validate(validators.isIP());
+            new model({text: "2001:db8:0000:1:1:1:1:1"}).save(function (err) {
+                assert.equal(err, null);
+                done();
+            });
+        });
+
+        it('Should fail string with invalid IP and option', function (done) {
+            items.path('text').validate(validators.isIP(4));
+            new model({text: "2001:db8:0000:1:1:1:1:1"}).save(function (err) {
+                assert.equal(err.message, "Validation failed");
+                done();
+            });
+        });
+
+        it('Should fail string with invalid IP', function (done) {
+            items.path('text').validate(validators.isIP());
+            new model({text: "cuicca"}).save(function (err) {
+                assert.equal(err.message, "Validation failed");
+                done();
+            });
+        });
+    });
+
+    describe('.isAlpha', function() {
+        it('Should pass valid string', function (done) {
+            items.path('text').validate(validators.isAlpha());
+            new model({text: "cuicca"}).save(function (err) {
+                assert.equal(err, null);
+                done();
+            });
+        });
+
+        it('Should fail invalid string', function (done) {
+            items.path('text').validate(validators.isAlpha());
+            new model({text: "cu1cca"}).save(function (err) {
+                assert.equal(err.message, "Validation failed");
+                done();
+            });
+        });
+    });
+
+    describe('Custom error message', function() {
+        it('Should return custom error message', function (done) {
+            items.path('text').validate(validators.isNumeric({message: "custom error message"}));
+            new model({text: "cu1cca"}).save(function (err) {
+                assert.equal(err.errors.text.message, "custom error message");
+                done();
+            });
+        });
+
+        it('Should return default error message', function (done) {
+            items.path('text').validate(validators.isAlphanumeric({message: null}));
+            new model({text: "cu_cca"}).save(function (err) {
+                assert.equal(err.errors.text.message, 'Validator failed for path `text` with value `cu_cca`');
+                done();
+            });
+        });
+    });
 });
